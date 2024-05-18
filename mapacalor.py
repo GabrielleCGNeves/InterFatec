96fce5317778ff68f8f2371f50ebd089e310ad53
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2024-05-18 17:41:56 -0300

    refactor with lists

--- mapacalor.py
+++ mapacalor.py
@@ -1,38 +1,46 @@
-from functools import reduce
-
 n_usuarios = int(input())
-superior = 0
-centro = 0
-esquerda = 0
-direita = 0
-inferior = 0
+
+regioes = [0, 0, 0, 0, 0]
 resultado = ""
 
 for i in range(n_usuarios):
     superior_input = input()
-    qtd_superior = superior_input.split(" ")
-    qtd_superior = reduce(lambda x, y: int(x) + int(y), qtd_superior)
+    qtd_superior = 0
+    qtd_inferior = 0
+    qtd_superior_temp = superior_input.split(" ")
 
-    superior += qtd_superior
+    for num in qtd_superior_temp:
+        qtd_superior += int(num)
+
+    regioes[0] += qtd_superior
 
     for j in range(4):
         corpo = input().split(" ")
-        esquerda += int(corpo[0])
-        centro += int(corpo[1])
-        direita += int(corpo[2])
+        regioes[1] += int(corpo[0])
+        regioes[2] += int(corpo[1])
+        regioes[3] += int(corpo[2])
 
 
     inferior_input = input()
-    qtd_inferior = inferior_input.split(" ")
-    qtd_inferior = reduce(lambda x, y: int(x) + int(y), qtd_inferior)
+    qtd_inferior_temp = inferior_input.split(" ")
 
-    inferior += qtd_inferior
+    for num in qtd_inferior_temp:
+        qtd_inferior += int(num)
 
-# print(f"superior: {superior}")
-# print(f"esquerda: {esquerda}")
-# print(f"centro: {centro}")
-# print(f"direita: {direita}")
-# print(f"inferior: {inferior}")
+    regioes[4] += qtd_inferior
 
+maior_valor = max(regioes)
+maior_regiao = regioes.index(maior_valor)
+
+if maior_regiao == 0:
+    resultado = "superior"
+if maior_regiao == 1:
+    resultado = "esquerda"
+if maior_regiao == 2:
+    resultado = "centro"
+if maior_regiao == 3:
+    resultado = "direita"
+if maior_regiao == 4:
+    resultado = "inferior"
 
 print(resultado)
